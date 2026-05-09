3d0884199cc46de54029d70f9c86021de3723306
Erick Pereira <erckeps.dms24@gmail.com>
2026-05-09 20:06:12 -0300

    Segundo commit - Desafio tabuleiro de xadrez

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        int L = int.Parse(Console.ReadLine()!);
+        int C = int.Parse(Console.ReadLine()!);
+
+        if ((L+C) % 2 ==0)
+        Console.WriteLine(1);
+        else
+        Console.WriteLine(0);
+    }
+}
